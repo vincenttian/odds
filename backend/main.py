b919e3032b2c4e56a29797f39457dba3399816c1
Vincent Tian <vincent@whatnot.com>
2024-10-01 23:33:42 -0400

    More cleanup.

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,18 @@
-from enum import Enum
-from typing import List
-
-import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
 from models import User, Base
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
+import uvicorn
 
-from notificationSender import send_message
 from db import (
     insertItem,
 )
 
-import os
-
 DATABASE_URL = os.environ["DATABASE_URL"]
 
 app = FastAPI()
-
 
 # Add this block after creating the FastAPI app instance
 app.add_middleware(
@@ -31,32 +23,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class itemTypes(Enum):
-    ANNOUNCEMENTS = "announcements"
-    EVENTS = "events"
-
-
-class teamColors(Enum):
-    RED = "red"
-    GREEN = "green"
-    BLUE = "blue"
-    YELLOW = "yellow"
-    NONE = "none"
-
-
-class user(BaseModel):
-    deviceID: str
-    name: str
-    teamColor: teamColors
-    pushToken: str = None
-    gpa: float = None
-
-# @app.get("/")
-# def get():
-#     return {"Status": True, "Version": "1.3.6", "sportsday": True}
-
 @app.get("/")
 def get_users():
+    # return {"Status": True, "Version": "1.3.6", "sportsday": True}
     engine = create_engine(DATABASE_URL)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -68,31 +37,6 @@ def get_users():
     finally:
         session.commit()
         session.close()
-
-@app.post("/pushNotification/")
-async def push(token: str, title: str, message: str, itemType: itemTypes):
-    if itemType == itemTypes.ANNOUNCEMENTS:
-        page = {"Link": "MainTab/News"}
-    elif itemType == itemTypes.EVENTS:
-        page = {"Link": "MainTab/Team Color"}
-    else:
-        page = None
-    send_message(token, title, message, data=page)
-    return "Success"
-
-
-@app.post("/pushNotificationAll/")
-async def pushall(title: str, message: str, itemType: itemTypes):
-    if itemType == itemTypes.ANNOUNCEMENTS:
-        page = {"Link": "MainTab/News"}
-    elif itemType == itemTypes.EVENTS:
-        page = {"Link": "MainTab/Team Color"}
-    else:
-        page = None
-    for pushTokens in splitArr(listPushTokens(), 10):
-        send_message(pushTokens, title, message, data=page)
-    return "Success"
-
 
 
 if __name__ == "__main__":
