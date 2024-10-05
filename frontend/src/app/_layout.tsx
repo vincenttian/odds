9a0d9a19f6a03cb91a91f24014b7f078fb0264b7
Vincent Tian <vincenttian16@gmail.com>
2024-10-05 10:24:51 -0400

    Set up redux.

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -6,6 +6,25 @@ import PropTypes from "prop-types";
 import styled, { ThemeProvider, type DefaultTheme } from 'styled-components/native'
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { appTheme, navTheme } from 'src/config/theme'
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const initialState = { value: 0 }
+function rootReducer(state = initialState, action: any) {
+  // Check to see if the reducer cares about this action
+  if (action.type === 'counter/increment') {
+    return {
+      ...state, // If so, make a copy of `state`
+      value: state.value + 1 // and update the copy with the new value
+    }
+  }
+  // otherwise return the existing state unchanged
+  return state
+}
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5500/graphql',
@@ -31,16 +50,18 @@ const client = new ApolloClient({
 
 export default function AppLayout() {
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={appTheme as DefaultTheme}>
-        <StatusBar style="light" />
-        <S.AppWrapper>
-          <NavProvider value={navTheme}>
-            <Slot screenOptions={{ headerShown: false }} />
-          </NavProvider>
-        </S.AppWrapper>
-      </ThemeProvider>
-    </ApolloProvider>
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={appTheme as DefaultTheme}>
+          <StatusBar style="light" />
+          <S.AppWrapper>
+            <NavProvider value={navTheme}>
+              <Slot screenOptions={{ headerShown: false }} />
+            </NavProvider>
+          </S.AppWrapper>
+        </ThemeProvider>
+      </ApolloProvider>
+    </Provider>
   )
 }
 
